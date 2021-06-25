9e8982cd950995f1388ec4417272439cda1fc626
Iniyan Arul <iniyan.m@krazybee.com>
2021-06-25 17:54:06 +0530

    jks update

--- app/src/main/java/com/iniyan/mvvm/data/network/MyApi.kt
+++ app/src/main/java/com/iniyan/mvvm/data/network/MyApi.kt
@@ -3,6 +3,8 @@ package com.iniyan.mvvm.data.network
 import com.iniyan.mvvm.data.network.responses.AuthResponse
 import com.iniyan.mvvm.data.network.responses.QuotesResponse
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,22 +12,24 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
+
 
 interface MyApi {
 
     @FormUrlEncoded
     @POST("login")
     suspend fun userLogin(
-        @Field("email") email: String,
-        @Field("password") password: String
+            @Field("email") email: String,
+            @Field("password") password: String
     ) : Response<AuthResponse>
 
     @FormUrlEncoded
     @POST("signup")
     suspend fun userSignup(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field("password") password: String
+            @Field("name") name: String,
+            @Field("email") email: String,
+            @Field("password") password: String
     ) : Response<AuthResponse>
 
     @GET("quotes")
@@ -33,19 +37,26 @@ interface MyApi {
 
     companion object{
         operator fun invoke(
-            networkConnectionInterceptor: NetworkConnectionInterceptor
+                networkConnectionInterceptor: NetworkConnectionInterceptor
         ) : MyApi {
 
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(Level.BODY)
+
             val okkHttpclient = OkHttpClient.Builder()
-                .addInterceptor(networkConnectionInterceptor)
-                .build()
+                    .addInterceptor(logging)
+                    .addInterceptor(networkConnectionInterceptor)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build()
 
             return Retrofit.Builder()
-                .client(okkHttpclient)
-                .baseUrl("https://api.simplifiedcoding.in/course-apis/mvvm/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(MyApi::class.java)
+                    .client(okkHttpclient)
+                    .baseUrl("https://api.simplifiedcoding.in/course-apis/mvvm/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(MyApi::class.java)
+
         }
     }
 
--- app/src/main/java/com/iniyan/mvvm/util/Exceptions.kt
+++ app/src/main/java/com/iniyan/mvvm/util/Exceptions.kt
@@ -1,9 +1,9 @@
 package com.iniyan.mvvm.util
 
 import java.io.IOException
+import java.lang.Exception
 
 
-
-class ApiException(message: String) : IOException(message)
+class ApiException(message: String) : Exception(message)
 
 class NoInternetException(message: String) : IOException(message)
